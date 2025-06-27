a5fe6401d2c1eaa6587c9688faa1f0983549365d
pyto-p <codebypytop@gmail.com>
2025-06-27 17:03:25 +0800

    feat: created User model

--- api/urls.py
+++ api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth/', include("authentication.urls")),
+    path('auth/', include("apps.authentication.urls")),
     path('admin/', admin.site.urls),
 ]
--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # No password field as AbstractUser already includes it
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    bio = models.TextField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
